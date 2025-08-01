356061202ecfc4a9d1e28746fcb41f76856eb8b5
DAAC-A00 <wnsdudrkwh@gmail.com>
2025-08-01 11:24:02 +0900

    theme light&dark 전환 기능 추가

---
+++ lib/theme_provider.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'theme_provider.g.dart';
+
+@riverpod
+class ThemeModeNotifier extends _$ThemeModeNotifier {
+  @override
+  ThemeMode build() {
+    return ThemeMode.system;
+  }
+
+  void toggleTheme() {
+    state = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+  }
+}
